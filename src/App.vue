f3e5977a521981220e70a4091b75129d16fc7919
986023997 <986023997@qq.com>
2020-04-06 15:34:58 +0800

    图片

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
+
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <main-tab-bar>
+      <tab-item>
+        <img >
+        <div slot="item-name">首页</div>
+      </tab-item>
+      <tab-item>
+        <div slot="item-name">分类</div>
+      </tab-item>
+      <tab-item>
+        <div slot="item-name">购物车</div>
+      </tab-item>
+      <tab-item>
+        <div slot="item-name">我的</div>
+      </tab-item>
+    </main-tab-bar>
   </div>
 </template>
+<script>
+  import mainTabBar from "./components/tabBar/mainTabBar";
+  import TabItem from "./components/tabBar/tabItem";
+  export default {
+    name:"App",
+    components:{
+      TabItem,
+      mainTabBar
+    }
+  }
 
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+
+body{
+  padding: 0px;
+  margin: 0px;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/tabBar/tabItem.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="tab-item">
+        <slot name="item-icon"></slot>
+        <slot name="item-name"></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "tabItem"
+    }
+</script>
+
+<style scoped>
+    .tab-item{
+        flex: 1;
+        height: 49px;
+        text-align: center;
+
+    }
+
+</style>
